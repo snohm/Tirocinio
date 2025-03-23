22563912093531affa6f2c39c52cbdba9deb24e4
snohm <alessandro.beneggi@gmail.com>
2025-03-23 11:47:59 +0100

    popola relazione articles

---
+++ script/articles.py
@@ -0,0 +1,27 @@
+import psycopg2
+import utils
+import os
+import rispy
+
+conn, cursor = utils.dbConn(dotenv_path="dbconn.env", search_path="agroann")
+try:
+    for file in os.listdir('data/articles'):
+        file = os.path.join('data/articles', file)
+        with open(file, 'r', encoding='utf-8') as f:
+            entries = rispy.load(f)
+            for art in entries:
+                if 'abstract' in art:
+                    cursor.execute("INSERT INTO articles (doi, title, abstract, url) VALUES (%s, %s, %s, %s)", (art['doi'], art['title'], art['abstract'], art['urls']))
+
+    conn.commit()
+    print("Dati inseriti correttamente!")
+
+except psycopg2.Error as e:
+    print(f"Errore nell'inserimento dati: {e}")
+    conn.rollback()
+except Exception as e:
+    print(e)
+
+finally:
+    cursor.close()
+    conn.close()
