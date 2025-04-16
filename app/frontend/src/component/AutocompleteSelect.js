4d33297feaece7b6a13d18540c80569cde9dd4cf
snohm <alessandro.beneggi@gmail.com>
2025-04-16 12:05:49 +0200

    suggerisce ent in base a input

---
+++ app/frontend/src/component/AutocompleteSelect.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import AsyncSelect from 'react-select/async';
+
+async function loadOptions(inputValue){
+    if (inputValue.length < 2) return [];
+
+    try {
+      const res = await fetch(`http://localhost:5000/api/ent?${(inputValue)}`);
+      const data = await res.json();
+      return data.map((item) => ({
+        label: item,
+        value: item
+      }));
+    } catch (err) {
+      console.error('Err fetching entities:', err);
+      return [];
+    }
+};
+
+function AutocompleteSelect({ onSelectionChange }) {
+  function handleChange (selectedOptions) {
+    const opt = selectedOptions ? selectedOptions.map((o) => o.value) : [];
+    onSelectionChange(opt);
+  };
+
+  return (
+    <AsyncSelect
+      isMulti
+      cacheOptions
+      defaultOptions={[]}
+      loadOptions={loadOptions}
+      onChange={handleChange}
+      placeholder="Search and select..."
+    />
+  );
+};
+
+export default AutocompleteSelect;
