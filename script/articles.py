04968f8515e254126a9cf6086d79ca0528e25c82
snohm <alessandro.beneggi@gmail.com>
2025-04-16 16:19:18 +0200

    fix estrazione url

--- script/articles.py
+++ script/articles.py
@@ -15,7 +15,8 @@ for file in os.listdir('data/articles'):
         entries = rispy.load(f)
         for art in tqdm(entries):
             if 'abstract' in art:
-                cursor.execute("INSERT INTO articles (doi, title, abstract, url) VALUES (%s, %s, %s, %s)", (art.get('doi'), art.get('title'), art.get('abstract'), art.get('urls')))
+                url = art.get('urls')[0].strip('{}') if art.get('urls') else None
+                cursor.execute("INSERT INTO articles (doi, title, abstract, url) VALUES (%s, %s, %s, %s)", (art.get('doi'), art.get('title'), art.get('abstract'), url))
 
 conn.commit()
 cursor.close()
