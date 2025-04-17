9a544b0851a3848763ee6b1af37897e86f6cede2
snohm <alessandro.beneggi@gmail.com>
2025-04-17 12:34:40 +0200

    spostato doi

--- app/frontend/src/component/ArticleDisplay.js
+++ app/frontend/src/component/ArticleDisplay.js
@@ -3,20 +3,20 @@ import DataTable from 'react-data-table-component';
 
 const customStyles = {
     headCells: {
-      style: {
-        fontSize: '16px',
-        fontWeight: 'bold',
-        fontFamily: 'Arial, sans-serif'
-      },
+        style: {
+            fontSize: '16px',
+            fontWeight: 'bold',
+            fontFamily: 'Arial, sans-serif'
+        },
     },
     cells: {
-      style: {
-        fontSize: '14px',
-        fontFamily: 'Arial, sans-serif'
-      },
+        style: {
+            fontSize: '14px',
+            fontFamily: 'Arial, sans-serif'
+        },
     },
-  };
-  
+};
+
 
 function ArticleDisplay({ data }) {
     if (Object.keys(data).length === 0) {
@@ -38,19 +38,15 @@ function ArticleDisplay({ data }) {
 
     const columns = [
         { name: 'Title', 
-            selector: row => row.title, 
+            selector: row => row.title,
             wrap: true },
-        { name: 'DOI', selector: row => row.doi,
-            width:'15%',
-            hide: 'md'
-        },
         { name: 'Link',
             selector: row => row.url,
             cell: row => <a href={row.url} target="_blank">Go to</a>,
-            width:'10%'
+            width: '70px'
         },
         { name: 
-            'Entities', selector: row => row.entities, 
+            'Entities', selector: row => row.entities,
             wrap: true,
             hide:'sm'
         }
@@ -58,8 +54,14 @@ function ArticleDisplay({ data }) {
 
     const ExpandableComponent = ({ data }) => (
         <div style={{ padding: '1rem', background: '#f9f9f9' }}>
-            <strong>Abstract:</strong>
-            <p>{data.abstract}</p>
+            <div>
+                <strong>Doi:</strong>
+                <p>{data.doi}</p>
+            </div>
+            <div>
+                <strong>Abstract:</strong>
+                <p>{data.abstract}</p>
+            </div>
         </div>
     );
 
