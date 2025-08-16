e754833e7f046495648815df4224124db863b7c0
snohm <alessandro.beneggi@gmail.com>
2025-08-16 10:18:59 +0200

    ref: fetch from app.js to arricleDisplay.js

--- app/frontend/src/component/App.js
+++ app/frontend/src/component/App.js
@@ -1,30 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import {useState} from 'react';
 import AutocompleteSelect from './AutocompleteSelect';
 import ArticleDisplay from './ArticleDisplay';
 import './css/app.css';
 
 function App() {
   const [searchItems, setSearchItems] = useState([]);
-  const [results, setResults] = useState({});
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    setResults({});
-    const query = searchItems.join('&');
-    const getArt = async () => {
-      try {
-        const response = await fetch(`http://localhost:5000/api/art?${query}`);
-        const data = await response.json();
-        setResults(data);
-        setLoading(false);
-      } catch (err) {
-        console.error('Error fetching data:', err);
-        setLoading(false);
-      }
-    };
-    getArt();
-    }, [searchItems]);
   
   return (
     <>
@@ -35,7 +15,7 @@ function App() {
         </div>
       </div>
       <div className='table'>
-        <ArticleDisplay data={results} loading={loading} addToSearch={setSearchItems} />
+        <ArticleDisplay addToSearch={setSearchItems} searchItems={searchItems} />
       </div>
     </>
   );
--- app/frontend/src/component/ArticleDisplay.js
+++ app/frontend/src/component/ArticleDisplay.js
@@ -1,13 +1,32 @@
-import React from 'react';
 import DataTable from 'react-data-table-component';
 import { useState, useEffect } from 'react';
 import './css/table.css';
 import { customStyles, columns, conditionalRowStyles } from './customTable';
 import ExpandableComponent from './ExpandableComponent';
 
-function ArticleDisplay({ data, loading, addToSearch }) {
+function ArticleDisplay({addToSearch, searchItems }) {
     const [resetPagination, setResetPagination] = useState(false);
     const [rows, setRows] = useState([]);
+    const [data, setData] = useState({});
+    const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setData({});
+    const query = searchItems.join('&');
+    const getArt = async () => {
+      try {
+        const response = await fetch(`http://localhost:5000/v2/api/art?${query}`);
+        const art = await response.json();
+        setData(art);
+        setLoading(false);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+        setLoading(false);
+      }
+    };
+    getArt();
+    }, [searchItems]);
 
     useEffect(() => {
         if (Object.keys(data).length === 0) {
