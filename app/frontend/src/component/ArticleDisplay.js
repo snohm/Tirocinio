5eed6f3fa195bf284dc599318b4b92c1b9fd8f78
snohm <alessandro.beneggi@gmail.com>
2025-08-17 20:30:22 +0200

    fetch derivated articles and updating table

--- app/frontend/src/component/ArticleDisplay.js
+++ app/frontend/src/component/ArticleDisplay.js
@@ -9,6 +9,7 @@ export default function ArticleDisplay({ addToSearch, searchItems }) {
     const [resetPagination, setResetPagination] = useState(false);
     const [rows, setRows] = useState([]);
     const { data: art, loading } = useFetch(`/v2/api/art?${searchItems.join('&')}`);
+    const { data: der_art } = useFetch(`/api/derivated_art?${searchItems.join('&')}`);
 
     useEffect(() => {
         if (Object.keys(art).length === 0) {
@@ -34,6 +35,28 @@ export default function ArticleDisplay({ addToSearch, searchItems }) {
         setRows(newRows);
         setResetPagination(prev => !prev);
     }, [art]);
+
+    useEffect(() => {
+        if (Object.keys(der_art).length === 0) {
+            return;
+        }
+
+        const { display_order, art_info, art2ent } = der_art;
+        const newRows = display_order.map((id, idx) => {
+            const info = art_info[id];
+            const ents = art2ent[id];
+            return {
+                id,
+                index: rows.length + idx + 1,
+                title: info.title,
+                doi: info.doi,
+                url: info.url,
+                abstract: info.abstract,
+                entities: ents.join(', ')
+            };
+        });
+        setRows(prevRows => [...prevRows, ...newRows]);
+    }, [der_art]);
 
     return (
         <DataTable
