b767a8c2e8f33f5a19b843cc57c3b77ca0049c4d
snohm <alessandro.beneggi@gmail.com>
2025-09-08 12:57:28 +0200

    use conn pool

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,41 +1,47 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import os
-import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-from functions.backend import get_art_by_ent, get_art_info
-from utils.dbConn import dbConn
+from utils.functions import get_art_by_ent, get_art_info
+from utils.dbPool import dbPool
 
 app = Flask(__name__)
 CORS(app)
 
+pool = dbPool(2, 5, '../../dbConn.env')
+search_path = 'agroann'
+
 @app.get("/api/ent")
 def ent():
-    conn, cursor = dbConn('../../dbConn.env', search_path="agroann")
+    conn = pool.getconn()
+    cursor = conn.cursor()
+    cursor.execute(f'SET search_path TO {search_path}')
     param = request.args.get('ent_name')
     if not param: param = ''
     cursor.execute("SELECT name FROM entities WHERE name ILIKE %s ORDER BY name ", ('%'+ param + '%',))
     ent = cursor.fetchall()
-    conn.close()
     cursor.close()
+    pool.putconn(conn)
     return jsonify([e[0] for e in ent]), 200
 
 @app.get("/v2/api/art")
 def art_v2():
-    conn, cursor = dbConn('../../dbConn.env', search_path="agroann")
+    conn = pool.getconn()
+    cursor = conn.cursor()
+    cursor.execute(f'SET search_path TO {search_path}')
     param = list(request.args.to_dict().keys())
     try:
         art2ent = get_art_by_ent(cursor, param)
     except ValueError as e:
         return jsonify(str(e)), 404
     art_info = get_art_info(cursor, list(art2ent.keys()))
-    conn.close()
     cursor.close()
+    pool.putconn(conn)
     return jsonify({"display_order": list(art2ent.keys()),"art2ent": art2ent, "art_info": art_info}), 200
 
 @app.get("/api/derivated_art")
 def der_art():
-    conn, cursor = dbConn('../../dbConn.env', search_path="agroann")
+    conn = pool.getconn()
+    cursor = conn.cursor()
+    cursor.execute(f'SET search_path TO {search_path}')
     param = list(request.args.to_dict().keys())
     try:
         art2ent = get_art_by_ent(cursor, param)
@@ -43,13 +49,15 @@ def der_art():
         return jsonify(str(e)), 404
     der_art = get_art_by_ent(cursor, param, keys=list(art2ent.keys()))
     art_info = get_art_info(cursor, list(der_art.keys()))
-    conn.close()
     cursor.close()
+    pool.putconn(conn)
     return jsonify({"display_order": list(der_art.keys()),"art2ent": der_art, "art_info": art_info}), 200
 
 @app.get("/api/art")
 def art():
-    conn, cursor = dbConn('../../dbConn.env', search_path="agroann")
+    conn = pool.getconn()
+    cursor = conn.cursor()
+    cursor.execute(f'SET search_path TO {search_path}')
     param = list(request.args.to_dict().keys())
     try:
         art2ent = get_art_by_ent(cursor, param)
@@ -58,17 +66,19 @@ def art():
     der_art = get_art_by_ent(cursor, param, keys=list(art2ent.keys()))
     art2ent.update(der_art)
     art_info = get_art_info(cursor, list(art2ent.keys()))
-    conn.close()
     cursor.close()
+    pool.putconn(conn)
     return jsonify({"display_order": list(art2ent.keys()),"art2ent": art2ent, "art_info": art_info}), 200
 
 @app.get("/api/art/<art_id>/related_ent")
 def related_ent(art_id):
-    conn, cursor = dbConn('../../dbConn.env', search_path="agroann")
+    conn = pool.getconn()
+    cursor = conn.cursor()
+    cursor.execute(f'SET search_path TO {search_path}')
     cursor.execute("SELECT e.name FROM mapping m JOIN entities e ON e.id = m.id_entities WHERE m.id_articles = %s", (art_id,))
     ent = cursor.fetchall()
-    conn.close()
     cursor.close()
+    pool.putconn(conn)
     return jsonify([e[0] for e in ent]), 200
 
 if __name__ == "__main__":
